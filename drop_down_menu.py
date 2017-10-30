6cc3e2e9bd7d740b06ee0279bfb6a4dd1879dc65
Ayush Saluja <ayush571995@outlook.com>
2017-10-30 23:25:46 +0530

    toolbar added

--- drop_down_menu.py
+++ drop_down_menu.py
@@ -14,7 +14,16 @@ submenu.add_command(label='New Project...', command=donothing)
 submenu.add_command(label='New', command=donothing)
 submenu.add_separator()
 submenu.add_command(label='Exit', command=root.quit)
-edit_menu=Menu(menu)
+edit_menu = Menu(menu)
 menu.add_cascade(label='Edit', menu=edit_menu)
 edit_menu.add_command(label='Redo', command=donothing)
+
+# **** Toolbar *****
+
+toolbar = Frame(root, bg="blue")
+insertBut = Button(toolbar, text="Insert Image", command=donothing)
+insertBut.pack(side=LEFT, padx=2, pady=2)
+print_but = Button(toolbar, text="Print", command=donothing)
+print_but.pack(side=LEFT, padx=2, pady=2)
+toolbar.pack(side=TOP, fill=X)
 root.mainloop()
